a1ea4a9af117ce24c188496baf009e66845cacee
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2025-07-02 09:56:21 +0700

    Fix Fatal error

--- flasx/main.py
+++ flasx/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-
 from . import routers
 
 app = FastAPI()
 app.include_router(routers.router)
+
+# Print docs URL on startup
+@app.on_event("startup")
+async def show_docs_url():
+    print("\nYour FastAPI docs are available at: http://127.0.0.1:8000/docs\n")
 
 
 @app.get("/")
