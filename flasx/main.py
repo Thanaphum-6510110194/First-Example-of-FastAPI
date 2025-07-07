a71293f3061d99c437c78d0e6405676a1f1f9a1c
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2025-07-07 09:23:32 +0700

    Import pydantic in main.py

--- flasx/main.py
+++ flasx/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from . import routers
+
+class RootResponse(BaseModel):
+    message: str
 
 app = FastAPI()
 app.include_router(routers.router)
 
-# Print docs URL on startup
-@app.on_event("startup")
-async def show_docs_url():
-    print("\nYour FastAPI docs are available at: http://127.0.0.1:8000/docs\n")
-
-
-@app.get("/")
-def read_root() -> dict:
-    return {"Hello": "World"}
+@app.get("/", response_model=RootResponse) 
+def read_root() -> RootResponse: 
+    return RootResponse(message="Hello World") 
