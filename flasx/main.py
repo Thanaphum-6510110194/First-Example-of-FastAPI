42c0583732b4f03aa86de5568aea515fd3421b08
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2025-07-07 10:26:59 +0700

    Add SQL model

--- flasx/main.py
+++ flasx/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from . import routers
+from . import models
 
 class RootResponse(BaseModel):
     message: str
 
 app = FastAPI()
 app.include_router(routers.router)
+
+models.create_db_and_tables()
 
 @app.get("/", response_model=RootResponse) 
 def read_root() -> RootResponse: 
---
+++ flasx/routers/models/__init__.py
@@ -0,0 +1,12 @@
+from .receiver_model import *
+from .item_model import *
+from sqlmodel import SQLModel, create_engine
+
+
+engine = create_engine("sqlite:///database.db")
+
+
+def create_db_and_tables():
+    """Create the database and tables."""
+    SQLModel.metadata.create_all(engine)
+    print("Database and tables created successfully.")
